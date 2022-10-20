fc297e0f1f349e84a434905fad8cdb7116d90df2
Zehedina Smajlovic <usatransport16@gmail.com>
2022-10-20 09:30:18 -0600

    commit tree fa96e1574ef3df8dea859a62bbfb2f385b851198

---
+++ index.js
@@ -0,0 +1,21 @@
+
+function receivesAFunction (callback) {
+    callback();
+}
+function returnsANamedFunction (){
+    return receivesAFunction;
+}
+function returnsAnAnonymousFunction(){
+    return function () {return 'Hi'};
+}
+
+
+
+
+
+
+
+
+
+
+    
